21fd5b984fa7cf135efd13272be0f5c9f0ea07c8
Erfan Dehghani <erfan.dehghani67@yahoo.com>
2025-11-26 17:39:06 +0330

    Debug:
     - A get api was in the wrong file

---
+++ src/app/api/user/project/[projectId]/floors/route.ts
@@ -0,0 +1,16 @@
+import { getAllFloorsController } from "@/lib/floor/controller/GetFloorController";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(
+    request: NextRequest,
+    { params }: { params: { projectId: string } }
+) {
+    try {
+        return await getAllFloorsController(request, params.projectId);
+    } catch (error: any) {
+        return NextResponse.json(
+            { error: error.message || "Internal server error" },
+            { status: 500 }
+        );
+    }
+}
