c6deb9fa0430a07bd3a6addac14ca43046611e92
Erfan Dehghani <erfan.dehghani67@yahoo.com>
2026-01-27 14:10:50 +0330

    next conf update

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // Enable standalone output for optimized Docker builds
   output: "standalone",
+
+
+  basePath: "/newframe",
+  assetPrefix: "/newframe",
 };
 
 export default nextConfig;
